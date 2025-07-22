b7f8dad4d37f058cea1f766b4d802450ddc179f8
gbedekar489 <161075939+gbedekar489@users.noreply.github.com>
2025-07-22 11:34:19 -0700

    Update script.js

--- frequency-capping/scripts/script.js
+++ frequency-capping/scripts/script.js
@@ -1,1 +1,35 @@
+alloy("sendEvent", {
+  renderDecisions: true,
+  personalization: {
+    surfaces: [
+      "web://yourdomain.com/offers.html#offerContainer"
+    ]
+  }
+}).then(response => {
+  const container = document.getElementById("offerContainer");
+  const offers = [];
+
+  (response.propositions || []).forEach(p => {
+    offers.push(...(p.items || []));
+  });
+
+  if (!offers.length) {
+    container.innerHTML = "<p>No offers available at the moment.</p>";
+    return;
+  }
+
+  offers.forEach(item => {
+    const html = decodeHtml(item.data?.content || "");
+    const wrapper = document.createElement("div");
+    wrapper.className = "offer-item";
+    wrapper.innerHTML = html;
+    container.appendChild(wrapper);
+  });
+});
+
+function decodeHtml(html) {
+  const txt = document.createElement("textarea");
+  txt.innerHTML = html;
+  return txt.value;
+}
 
